a19e4fa1b9abe8c9987afab650920a4561ddec2c
Matthew Cool <matthewbcool@gmail.com>
2021-07-01 11:40:04 -0700

    hacked it in there

--- src/hub.js
+++ src/hub.js
@@ -119,6 +119,7 @@ import "./components/optional-alternative-to-not-hide";
 import "./components/avatar-audio-source";
 import "./components/avatar-inspect-collider";
 import "./components/video-texture-target";
+import "./components/three-sixty-image-switch";
 
 import ReactDOM from "react-dom";
 import React from "react";
